e6403929dcbf32f718091027a84513ac82cf8fbd
Facundo Vergara <fakumax@gmail.com>
2023-06-13 20:10:42 -0300

    Fix to Mobile Version 2.0

--- components/slices/SectionContentSlice/variants/default/defaultStyles.js
+++ components/slices/SectionContentSlice/variants/default/defaultStyles.js
@@ -5,40 +5,8 @@ export const Wrapper = styled.div`
   height: 600px;
   display: flex;
   background-color: ${(props) => props.color};
+  padding: 2rem 0;
   ///////////////////
-  .leftContainer {
-    height: 100%;
-    width: 20%;
-    /* background-color: red; */
-    display: flex;
-
-    .WhiteContainer {
-      position: relative;
-      height: 100%;
-      background-color: white;
-      flex: 1;
-    }
-  }
-  .ImageLeft {
-    display: flex;
-    height: 100%;
-    justify-content: end;
-    position: relative;
-    width: inherit;
-    img {
-      width: auto;
-      height: 100%;
-      object-fit: cover;
-      max-height: fit-content;
-      background: linear-gradient(
-        to right,
-        white 0%,
-        white 50%,
-        transparent 50%,
-        transparent 100%
-      );
-    }
-  }
 
   ///////////////
   .rightContainer {
@@ -49,7 +17,6 @@ export const Wrapper = styled.div`
     justify-content: center;
     .ImageContainer {
       position: relative;
-
       width: 100%;
       max-width: 160px;
       max-height: 200px;
@@ -77,6 +44,7 @@ export const Wrapper = styled.div`
   @media (max-width: ${(props) => props.theme.breakpoints.md}) {
     flex-direction: column;
     height: fit-content;
+    padding: 0;
     .ImageContainer {
       display: none;
     }
@@ -90,6 +58,9 @@ export const ContainerItems = styled.div`
   align-items: center;
   gap: 40px;
   padding: 40px;
+  &:last-child {
+    flex-direction: row-reverse;
+  }
 `;
 
 export const HeadTitle = styled.div`
@@ -101,5 +72,69 @@ export const HeadTitle = styled.div`
   .new1 {
     border: 1px solid #7c5a19;
     width: 50%;
+  }
+`;
+
+export const LeftContainer = styled.div`
+  height: 100%;
+  width: 20%;
+  /* background-color: red; */
+  display: flex;
+  .ImagePaper {
+    display: flex;
+    height: 100%;
+    justify-content: end;
+    position: relative;
+    width: inherit;
+    img {
+      width: auto;
+      height: 100%;
+      object-fit: cover;
+      max-height: fit-content;
+      background: linear-gradient(
+        to right,
+        white 0%,
+        white 50%,
+        transparent 50%,
+        transparent 100%
+      );
+    }
+  }
+  @media (max-width: ${(props) => props.theme.breakpoints.md}) {
+    width: 100%;
+    padding: 20px 0;
+    background: white;
+    .ImagePaper {
+      display: none;
+    }
+  }
+`;
+
+export const WhiteContainer = styled.div`
+  position: relative;
+  height: 100%;
+  background-color: white;
+  flex: 1;
+  text-align: justify;
+  padding: 0px 20px;
+  .ImageLeft {
+    max-width: 100px;
+    height: auto;
+    display: flex;
+    margin: 1rem auto;
+  }
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    max-height: fit-content;
+  }
+  .TextLeft {
+    h2 {
+      width: 100%;
+      text-align: center;
+      font-size: 24px;
+      line-height: 35px;
+    }
   }
 `;
