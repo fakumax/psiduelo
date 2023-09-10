8b27ef033da8442159a5026931a947fd9129e10e
Facundo Vergara <fakumax@gmail.com>
2023-09-10 15:05:41 -0300

    Header refactor

--- src/components/common/Header/Header.js
+++ src/components/common/Header/Header.js
@@ -1,50 +1,60 @@
 import { PrismicNextImage } from '@prismicio/next';
 import { PrismicLink, PrismicText } from '@prismicio/react';
 import { AiOutlineMenu } from 'react-icons/ai';
-import { IconBurger, LogoStyle, NavBar, RightContainer } from './HeaderStyle';
+import {
+  BgDiv,
+  IconBurger,
+  LeftContainer,
+  LogoStyle,
+  NavBar,
+  RightContainer,
+} from './HeaderStyle';
+import { useState } from 'react';
 
 export const Header = ({ navigation }) => {
   const { logoimage, navleft, navright, logolink } = navigation.data;
 
+  const [clicked, setClicked] = useState(false);
+  const handleClick = () => {
+    setClicked(!clicked);
+  };
+
   return (
-    <div>
-      <NavBar>
-        <RightContainer>
-          {/* NAVIGATION LEFT*/}
-          <div>
-            <ul>
-              {navleft.map((item, index) => (
-                <li key={index}>
-                  <PrismicLink field={item.link}>
-                    <PrismicText field={item.name} />
-                  </PrismicLink>
-                </li>
-              ))}
-            </ul>
-          </div>
-          {/* LOGO */}
-          <LogoStyle>
-            <PrismicLink field={logolink}>
-              <PrismicNextImage field={logoimage} priority={true} />
-            </PrismicLink>
-          </LogoStyle>
-          {/* NAVIGATION RIGHT*/}
-          <div style={{ width: 'min-content' }}>
-            <ul>
-              {navright.map((item, index) => (
-                <li key={index}>
-                  <PrismicLink field={item.link}>
-                    <PrismicText field={item.name} />
-                  </PrismicLink>
-                </li>
-              ))}
-            </ul>
-          </div>
-          <IconBurger>
-            <AiOutlineMenu size={25} />
-          </IconBurger>
-        </RightContainer>
-      </NavBar>
-    </div>
+    <NavBar>
+      {/* NAVIGATION RIGHT*/}
+      <RightContainer className={`links ${clicked ? 'active' : ''}`}>
+        <ul>
+          {navleft.map((item, index) => (
+            <li key={index}>
+              <PrismicLink onClick={handleClick} field={item.link}>
+                <PrismicText field={item.name} />
+              </PrismicLink>
+            </li>
+          ))}
+        </ul>
+      </RightContainer>
+      {/* LOGO */}
+      <LogoStyle>
+        <PrismicLink field={logolink}>
+          <PrismicNextImage field={logoimage} priority={true} />
+        </PrismicLink>
+      </LogoStyle>
+      {/* NAVIGATION LEFT*/}
+      <LeftContainer className={`links ${clicked ? 'active' : ''}`}>
+        <ul>
+          {navright.map((item, index) => (
+            <li key={index}>
+              <PrismicLink onClick={handleClick} field={item.link}>
+                <PrismicText field={item.name} />
+              </PrismicLink>
+            </li>
+          ))}
+        </ul>
+      </LeftContainer>
+      <IconBurger clicked={clicked} handleClick={handleClick}>
+        <AiOutlineMenu size={25} />
+      </IconBurger>
+      <BgDiv className={`initial ${clicked ? ' active' : ''}`}></BgDiv>
+    </NavBar>
   );
 };
