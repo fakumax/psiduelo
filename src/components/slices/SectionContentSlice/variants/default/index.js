bc46defee2ade480b67b6fd73c8ea231425a6799
Facundo Vergara <fakumax@gmail.com>
2023-11-06 23:18:55 -0300

    Fixed buttons initials

--- src/components/slices/SectionContentSlice/variants/default/index.js
+++ src/components/slices/SectionContentSlice/variants/default/index.js
@@ -1,6 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import { PrismicNextImage, PrismicNextLink } from '@prismicio/next';
-import { PrismicRichText, PrismicText } from '@prismicio/react';
+import { PrismicNextImage } from '@prismicio/next';
+import { PrismicLink, PrismicRichText } from '@prismicio/react';
 import {
   ContainerItems,
   HeadTitle,
@@ -9,39 +8,13 @@ import {
   WhiteContainer,
   Wrapper,
 } from './defaultStyles';
-import { ContentModal, CustomModal } from '@/components/common/Modal';
-import { fetchArticles } from '@/utils/fetchArticles';
 
 const Default = (slice) => {
-  const [articles, setArticles] = useState(null);
   //console.log(slice.items);
-  const { bgcolor, bgleft, titleleft, textleft, descripcionleft, imageleft, textbutton } =
+  //console.log(slice.primary);
+
+  const { bgcolor, bgleft, titleleft, textleft, descripcionleft, imageleft, textbutton,linkbutton } =
     slice.primary;
-
-  const [isModalOpen, setIsModalOpen] = useState(false); // Estado para controlar la apertura/cierre del modal
-  const [selectedItem, setSelectedItem] = useState(null);
-
-  const openModal = (uid) => {
-    const article = articles.find((article) => article.uid === uid);
-    setSelectedItem(article);
-    setIsModalOpen(true);
-  };
-
-  const closeModal = () => {
-    setIsModalOpen(false);
-    setSelectedItem(null);
-  };
-
-  useEffect(() => {
-    const fetchAndSetArticles = async () => {
-      const fetchedArticles = await fetchArticles();
-      setArticles(fetchedArticles);
-      console.log(fetchedArticles);
-    };
-
-    fetchAndSetArticles();
-  }, []);
-
   return (
     <Wrapper color={bgcolor}>
       <LeftContainer>
@@ -53,13 +26,7 @@ const Default = (slice) => {
             <PrismicRichText field={titleleft} />
             <PrismicRichText field={descripcionleft} />
           </div>
-          <button
-            onClick={() =>
-              openModal('en-alicia-en-el-pais-de-las-maravillas-alicia-le-pregunta')
-            }
-          >
-            {textbutton}
-          </button>
+                                    <PrismicLink field={linkbutton}> { textbutton}</PrismicLink>
         </WhiteContainer>
         <div className="ImagePaper">
           <PrismicNextImage field={bgleft} fallbackAlt="" />
@@ -68,7 +35,6 @@ const Default = (slice) => {
 
       <RightContainer>
         {slice.items.map((item, index) => {
-          const titleText = item.title[0].text;
           return (
             <ContainerItems key={index}>
               <div className="WrapperImage">
@@ -82,27 +48,7 @@ const Default = (slice) => {
                   <div className="new1" />
                 </HeadTitle>
                 <PrismicRichText field={item.description} />
-
-                <button
-                  onClick={() =>
-                    openModal(
-                      titleText === 'El Duelo Duele'
-                        ? 'el-duelo-duele'
-                        : 'por-que-es-importante-el-apoyo-psicologico-en-el-duelo'
-                    )
-                  }
-                >
-                  {item.textbutton}
-                </button>
-                <CustomModal
-                  isOpen={isModalOpen}
-                  onRequestClose={closeModal}
-                  content={
-                    <ContentModal
-                      data={selectedItem ? selectedItem.data.slices : undefined}
-                    />
-                  }
-                />
+                  <PrismicLink field={item.linkbutton}> {item.textbutton}</PrismicLink>
               </div>
             </ContainerItems>
           );
