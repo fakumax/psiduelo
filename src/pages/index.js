b012fce3e05123db7ffac05a1d736266a57b5204
Facundo Vergara <fakumax@n0makW11-PC>
2024-01-04 00:14:27 -0300

    Added loader

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,31 +5,63 @@ import * as prismicH from '@prismicio/helpers';
 import { createClient } from '@/prismicio';
 import { components } from '@/slices';
 import { Layout } from '@/components/common/Layout';
+import { useEffect, useState } from 'react';
+import { Loading } from '@/components/common/Loading/Loading';
 
 const Index = ({ page, navigation, copyright }) => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setTimeout(() => {
+        setLoading(false);
+      }, 2000);
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <Layout navigation={navigation} copyright={copyright}>
-      <Head>
-        <title>{prismicH.asText(page.data.title)}</title>
-      </Head>
-      <SliceZone slices={page.data.slices} components={components} />
-    </Layout>
+    <>
+      {loading ? (
+        <Loading />
+      ) : (
+        <Layout navigation={navigation} copyright={copyright}>
+          <Head>
+            <title>{prismicH.asText(page.data.title)}</title>
+          </Head>
+          <SliceZone slices={page.data.slices} components={components} />
+        </Layout>
+      )}
+    </>
   );
 };
 
 export default Index;
 
 export async function getStaticProps({ previewData }) {
-  const client = createClient({ previewData });
-  const page = await client.getByUID('page', 'home');
-  const navigation = await client.getSingle('navigation');
-  const copyright = await client.getSingle('copyright');
-
-  return {
-    props: {
-      page,
-      navigation,
-      copyright,
-    },
-  };
+  try {
+    const client = createClient({ previewData });
+    const [page, navigation, copyright] = await Promise.all([
+      client.getByUID('page', 'home'),
+      client.getSingle('navigation'),
+      client.getSingle('copyright'),
+    ]);
+    return {
+      props: {
+        page,
+        navigation,
+        copyright,
+      },
+    };
+  } catch (error) {
+    console.error('Error fetching data from Prismic', error);
+    return {
+      props: {
+        page: null,
+        navigation: null,
+        copyright: null,
+      },
+    };
+  }
 }
