06df3b2b5d4f22692ae565d26c3ac90dd87aa553
Facundo <fakumax@gmail.com>
2024-01-13 00:50:57 -0300

    Added react hook form and zod

--- src/components/slices/TextBlockSlice/variants/contactText/index.js
+++ src/components/slices/TextBlockSlice/variants/contactText/index.js
@@ -1,3 +1,17 @@
+import React from 'react';
+import { useForm, SubmitHandler } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { z } from 'zod';
+
+const phoneRegex = new RegExp(/^([+]?[\s0-9]+)?(\d{3}|[(]?[0-9]+[)])?([-]?[\s]?[0-9])+$/);
+
+const schema = z.object({
+  nombre: z.string().min(4, { message: 'Mínimo 4 caracteres' }),
+  email: z.string().email({ message: 'Correo electrónico no válido' }),
+  telefono: z.string().regex(phoneRegex, 'Invalid Number!'),
+  mensaje: z.string().min(10, { message: 'Mensaje obligatorio' }),
+});
+
 import {
   Container,
   Wrapper,
@@ -8,48 +22,54 @@ import {
   StyledTextarea,
   StyledSubmitButton,
 } from './defaultStyles.js';
+
 import { PrismicNextImage } from '@prismicio/next';
 
 const ContactText = (slice) => {
   console.log('--slice--TextBlockSlice', slice);
   const { name, email, phone, message, contact, image } = slice.primary;
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log('Formulario enviado!');
-    // Agrega aquí la lógica adicional para manejar el envío del formulario si es necesario
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(schema),
+  });
+
+  const onSubmit = (data) => {
+    console.log(data);
   };
 
   return (
     <Wrapper>
       <Container>
-        <div>
-          <p>{contact}</p>
-          {/* <ImageContainer>
-            <PrismicNextImage field={image} height={350} />
-          </ImageContainer> */}
-        </div>
-        <div>
-          <StyledLabel>
-            {name}:
-            <StyledInput type="text" name="name" />
-          </StyledLabel>
-          <StyledLabel>
-            {email}:
-            <StyledInput type="text" name="email" />
-          </StyledLabel>
-          <StyledLabel>
-            {phone}:
-            <StyledInput type="text" name="phone" />
-          </StyledLabel>
-          <StyledLabel>
-            {message}:
-            <StyledTextarea name="message" />
-          </StyledLabel>
-          <FormContainer>
-            <StyledSubmitButton type="submit" value="Enviar" onClick={handleSubmit} />
-          </FormContainer>
-        </div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div>
+            <label>{name}</label>
+            <input {...register('nombre')} />
+            <p>{errors.nombre?.message}</p>
+          </div>
+
+          <div>
+            <label>{email}</label>
+            <input {...register('email')} />
+            <p>{errors.email?.message}</p>
+          </div>
+
+          <div>
+            <label>{phone}</label>
+            <input {...register('telefono')} />
+            <p>{errors.telefono?.message}</p>
+          </div>
+
+          <div>
+            <label>{message}</label>
+            <textarea {...register('mensaje')} />
+            <p>{errors.mensaje?.message}</p>
+          </div>
+          <button type="submit">Enviar</button>
+        </form>
       </Container>
     </Wrapper>
   );
