f333d0d9c8f26a1b2c2e090da16d18386244a3ff
Facundo <fakumax@gmail.com>
2024-01-13 22:30:00 -0300

    Added email resend

--- src/pages/api/ResendEmail.js
+++ src/pages/api/ResendEmail.js
@@ -4,12 +4,12 @@ import { Resend } from 'resend';
 const resend = new Resend(process.env.RESEND_EMAIL);
 
 const ResendEmail = async (req, res) => {
+  console.log('REQUEST----->', req.body);
   const { name, email, phone, message } = req.body;
-
   const { data, error } = await resend.emails.send({
-    from: 'Acme <onboarding@resend.dev>',
+    from: 'Web PSIDUELO <onboarding@resend.dev>',
     to: ['fakumax@gmail.com'],
-    subject: 'Hello world',
+    subject: 'Mensaje nuevo | PSIDUELO',
     react: ResendEmailTemplate({
       name,
       email,
@@ -25,4 +25,4 @@ const ResendEmail = async (req, res) => {
   res.status(200).json(data);
 };
 
-export { ResendEmail };
+export default ResendEmail;
