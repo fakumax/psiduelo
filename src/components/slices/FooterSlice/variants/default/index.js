bf855f2de8f1ca13037cf8d5cec232249d9dde0b
Facundo V <fakumax@gmail.com>
2024-05-01 01:20:20 -0300

    Footer style

--- src/components/slices/FooterSlice/variants/default/index.js
+++ src/components/slices/FooterSlice/variants/default/index.js
@@ -1,6 +1,14 @@
 import { PrismicNextImage } from '@prismicio/next';
 import { PrismicRichText } from '@prismicio/react';
-import { BodyContainer, IconsGroup, Wrapper, Icon, Frase } from './defaultStyles';
+import {
+  BodyContainer,
+  IconsGroup,
+  Wrapper,
+  Icon,
+  Frase,
+  ContainerImagePlant,
+  Details,
+} from './defaultStyles';
 
 //Icons
 import {
@@ -14,29 +22,50 @@ import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import Link from 'next/link';
 
 const Default = (slice) => {
-  //console.log('--slice--Footer', slice);
-  const { bgimage, description, text, logo } = slice.primary;
+  console.log('--slice--Footer', slice);
+  const { description, text, logo, imageplant, papertop, paperbottom } = slice.primary;
+  console.log('papertop', papertop);
+  console.log('paperl', paperbottom);
   return (
-    <Wrapper bgimage={bgimage.url}>
-      <BodyContainer>
-        <PrismicNextImage field={logo} />
+    <Wrapper>
+      <div>
+        {/* <div>
+          <PrismicNextImage field={papertop} />
+        </div> */}
         <div>
-          <PrismicRichText field={text} />
-          <PrismicRichText field={description} />
+          <BodyContainer>
+            <div>
+              <div>
+                <PrismicNextImage field={logo} />
+              </div>
+              <ContainerImagePlant>
+                <PrismicNextImage field={imageplant} />
+              </ContainerImagePlant>
+            </div>
+
+            <Details>
+              <PrismicRichText field={text} />
+              <PrismicRichText field={description} />
+            </Details>
+          </BodyContainer>
+          <IconsGroup>
+            <Icon icon={faWhatsapp} />
+            <Icon icon={faInstagram} />
+            <Icon icon={faFacebookF} />
+            <Link href="mailto:destinatario@example.com">
+              <Icon icon={faEnvelope} />
+            </Link>
+            <Icon icon={faDiscord} />
+            <Link href="https://open.spotify.com/playlist/1yC7MAXP13XN1kcVC8PlE6?si=6bd0feb4da69407b">
+              <Icon icon={faSpotify} />
+            </Link>
+          </IconsGroup>
         </div>
-      </BodyContainer>
-      <IconsGroup>
-        <Icon icon={faWhatsapp} />
-        <Icon icon={faInstagram} />
-        <Icon icon={faFacebookF} />
-        <Link href="mailto:destinatario@example.com">
-          <Icon icon={faEnvelope} />
-        </Link>
-        <Icon icon={faDiscord} />
-        <Link href="https://open.spotify.com/playlist/1yC7MAXP13XN1kcVC8PlE6?si=6bd0feb4da69407b">
-          <Icon icon={faSpotify} />
-        </Link>
-      </IconsGroup>
+        {/* <div>
+          <PrismicNextImage field={paperbottom} />
+        </div> */}
+      </div>
+
       <Frase>
         <p>
           &quot;Algunas veces solíamos hablar de iniciar una familia...pero es
