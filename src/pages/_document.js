ddd93067afe148b32fb8d3607fc36da756544319
Facundo Vergara <fakumax@n0mak-Desktop>
2024-06-15 22:32:56 -0300

    Changes style articles

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,18 +5,49 @@ export default function Document() {
   return (
     <Html lang="es">
       <Head>
-        <meta name="description" content="Psicóloga Diplomada y Especialista en Duelo. Atención Psicológica Online a Adolescentes y Adultos hispanohablantes." />
-        <meta name="keywords" content="duelo, psicóloga, psicología, atención online, especialista" />
+        <meta
+          name="description"
+          content="Descubre cómo la Psicóloga Diplomada y Especialista en Duelo puede ayudarte con atención psicológica online. Ofrecemos terapias personalizadas para adolescentes y adultos hispanohablantes, enfocándonos en el proceso de duelo y otros desafíos emocionales. Contacta hoy mismo para una consulta virtual y empieza tu camino hacia la recuperación emocional."
+        />
+        <meta
+          name="keywords"
+          content="psicóloga, especialista en duelo, atención psicológica online, terapia online, psicología, duelo, terapia para adolescentes, terapia para adultos, consulta psicológica, apoyo emocional, salud mental, hispanohablantes, terapia de duelo, terapia personalizada, recuperación emocional"
+        />
         <meta name="author" content="Facundo Vergara" />
-        <link rel="icon" type="image/png" sizes="any" href="https://prismic.io/favicon.ico" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="any"
+          href="https://prismic.io/favicon.ico"
+        />
+
+        {/* Preconnect to external fonts */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link href="https://fonts.googleapis.com/css2?family=Old+Standard+TT:ital@1&display=swap" rel="stylesheet" />
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&display=swap" rel="stylesheet" />
-        <link href="https://fonts.googleapis.com/css2?family=La+Belle+Aurore&display=swap" rel="stylesheet" />
-        <link href="https://fonts.googleapis.com/css2?family=Crimson+Text:ital,wght@0,400;0,600;0,700;1,400&display=swap" rel="stylesheet" />
+
+        {/* Load critical fonts */}
+        <link
+          href="https://fonts.googleapis.com/css2?family=Old+Standard+TT:ital@1&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=La+Belle+Aurore&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Crimson+Text:ital,wght@0,400;0,600;0,700;1,400&display=swap"
+          rel="stylesheet"
+        />
+
         {/* Google Analytics */}
-        <script async src={`https://www.googletagmanager.com/gtag/js?id=${gaTrackingId}`}></script>
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${gaTrackingId}`}
+        ></script>
         <script
           dangerouslySetInnerHTML={{
             __html: `
